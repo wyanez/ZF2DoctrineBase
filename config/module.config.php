22dfa97b9d972bdd27a4d8804c2f872158384a99
William Yanez <wyanez@gmail.com>
2014-12-11 01:12:06 -0430

    Commit inicial. Creación del proyecto

---
+++ config/module.config.php
@@ -0,0 +1,30 @@
+<?php
+return array(
+
+   // 'controllers' => array(
+   //   'invokables' => array(),
+   // ),
+
+
+   // 'service_manager' => array(
+
+   //     'factories' => array(
+   //     ),
+
+    //    'initializers' => array(
+    //    ),
+    ),
+
+
+    //'view_manager' => array(
+    //    'template_path_stack' => array(
+    //        '' => __DIR__ . '/../view',
+    //    ),
+    //),
+
+
+    //'router' => array(
+    //    'routes' => array(        
+    //    ),
+    //),
+);
