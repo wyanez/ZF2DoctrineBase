cea35145ab93a4bc545742bb3dd4adc1dfe1629f
William Yanez <wyanez@gmail.com>
2018-01-31 00:10:55 -0400

    Refactorizado el View Helper RenderFlashMessenger()para trabajar con mensajes de tipo 'error' y 'warning' y mostrar los alert adecuados para Bootstrap3

--- src/ZF2DoctrineBase/View/Helper/RenderFlashMessenger.php
+++ src/ZF2DoctrineBase/View/Helper/RenderFlashMessenger.php
@@ -15,21 +15,30 @@
 
         // tipo: success,info,danger,warning
         public function __invoke($tipo='success'){
-            if ($this->getView()->flashMessenger()->hasMessages()){ 
-                $display_flash='true';
-            } 
-            else{
-                $display_flash='none';
-            } 
-
-            $str= "<div id='flash_msg' style='display: $display_flash'>";
-            $str.= "<div id='flash_msg_alert' class='alert alert-$tipo alert-dismissable'>";
-            $str.= "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>";
-            $str.= "<span id='flash_txt'>".$this->getView()->flashMessenger()->render()."</span>";
-            $str.= "</div>";
-            $str.= "</div>";
-
-            return $str; 
+            $html='';
+            if ($this->getView()->flashMessenger()->hasErrorMessages()){
+                $render_flash = $this->getView()->flashMessenger()->render('error'); 
+                $html .= $this->_renderHtmlFlashBs3('danger','true',$render_flash);
+            }
+            if ($this->getView()->flashMessenger()->hasWarningMessages()){
+                $render_flash = $this->getView()->flashMessenger()->render('warning');
+                $html .= $this->_renderHtmlFlashBs3('warning','true',$render_flash);
+            }
+            if ($this->getView()->flashMessenger()->hasMessages()){
+                $render_flash = $this->getView()->flashMessenger()->render();
+                $html .= $this->_renderHtmlFlashBs3('success','true',$render_flash);    
+            }
+            return $html;
+        }
+        
+        private function _renderHtmlFlashBs3($tipo,$display_flash,$render_flash){
+            $strHtml= "<div id='flash_msg' style='display: $display_flash'>";
+            $strHtml.= "<div id='flash_msg_alert' class='alert alert-$tipo alert-dismissable'>";
+            $strHtml.= "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>";
+            $strHtml.= "<span id='flash_txt'>".$render_flash."</span>";
+            $strHtml.= "</div>";
+            $strHtml.= "</div>";
+            return $strHtml;
         }
     }
 ?>
